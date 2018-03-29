fae5035621efaacb77f9839d7a27f2b05a36b4cd
Joshua Williams <williamsjr812@gmail.com>
2018-03-29 01:09:53 -0400

    removed blank lines

--- public_html/contact_handler.php
+++ public_html/contact_handler.php
@@ -1,5 +1,4 @@
 <?php
-
 if(isset($_POST["submit"])){
     $to = "josh@op-siix.com";
     $name = $_POST["Name"];
@@ -10,12 +9,10 @@ if(isset($_POST["submit"])){
         . "\n\nMessage: " . $_POST["message"];
     $message2 = "Thank you for contacting Op-Siix. Here is a copy of your message:"
         . "\n\n" . $name . "\n\nSubject: " . $subject . "\n\nMessage: " . $_POST["message"];
-
     $headers = "From: " . $from;
     $headers2 = "From: " . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2);
     echo "Message Sent. Thank you " . $name . ", we will contact you shortly.";
 }
-
 ?>
